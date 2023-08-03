589d9e32a8d802a9b6cfd51e85ee3901e6d1233b
Tyler J. Perry <iamtylerjordan@gmail.com>
2023-08-02 23:25:54 -0400

    feat:rendering footer social links

--- wp-content/themes/sc/footer.php
+++ wp-content/themes/sc/footer.php
@@ -15,17 +15,19 @@
 	<div class="footer__wrapper container-sm">
 		<div class="footer__links d-lg-flex justify-content-between">
 			<div class="footer__col">
-				<div class="social-links">
-					<ul>
-						<li><a href="https://www.facebook.com/SabrinaClaudioMusic" class="icon-social" target="_blank"><i class="fa-brands fa-facebook-f"></i></a></li>
-						<li><a href="https://twitter.com/SabrinaClaudio" class="icon-social" target="_blank"><i class="fa-brands fa-twitter"></i></a></li>
-						<li><a href="https://www.instagram.com/sabrinaclaudio/" class="icon-social" target="_blank"><i class="fa-brands fa-instagram"></i></a></li>
-						<li><a href="https://www.youtube.com/user/sabrinaclaudiomusic" class="icon-social" target="_blank"><i class="fa-brands fa-youtube"></i></a></li>
-						<li><a href="https://open.spotify.com/artist/30DhU7BDmF4PH0JVhu8ZRg" class="icon-social" target="_blank"><i class="fa-brands fa-spotify"></i></a></li>
-						<li><a href="https://music.apple.com/us/artist/sabrina-claudio/1128029190" class="icon-social" target="_blank"><i class="fa-brands fa-apple"></i></a></li>
-						<li><a href="https://www.tiktok.com/@sabrina" class="icon-social" target="_blank"><i class="fa-brands fa-tiktok"></i></a></li>
-					</ul>
-				</div>
+				<?php if(have_rows('social_links', 'option')):?>
+					<div class="social-links">
+						<ul>
+							<?php while (have_rows('social_links', 'option')): the_row();
+								$link_label = get_sub_field('label');
+								$link_url = get_sub_field('url');
+							?>
+								<li><a href="<?php echo $link_url;?>" class="icon-social" target="_blank"><i class="fa-brands fa-<?php echo $link_label;?>"></i></a></li>
+							<?php endwhile;?>
+							
+						</ul>
+					</div>
+				<?php endif;?>
 			</div>
 			<div class="footer__col footer__col-links">
 				<ul>
